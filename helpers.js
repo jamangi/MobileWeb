3e63f57176f56276a57d2d7828929d7bd78928df
rubyshadows <343@holbertonschool.com>
2018-10-02 12:02:55 -0700

    upgrade simple search

--- helpers.js
+++ helpers.js
@@ -8,3 +8,12 @@ function setValues(obj, left, top, width, height){
 function check(map, row, col){
 	return map.barrierList[row +'-'+col] === undefined;
 }
+
+class Node {
+	constructor(map, row, col) {
+		this.map = map, this.row = row, this.col = col;
+		if (this.map.search === undefined)
+			this.map.search = []
+	}
+	mark(){this.map.search[row][col] = true;}
+}
